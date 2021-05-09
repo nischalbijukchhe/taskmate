5f06267dd44549bdee3f363d454780ccc583951c
Nischal Bijukchhe <nischalbijukchhe8@gmail.com>
2021-05-09 14:48:55 +0400

    file updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
@@ -135,6 +135,8 @@ LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
 
 django_heroku.settings(locals())
+
+
 
 
 
